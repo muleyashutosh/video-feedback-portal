d1f398906be4209b5f75b15f434020ee2107fbf1
Ashutosh Muley <muleyashutosh@gmail.com>
2021-05-06 12:55:46 +0530

    video

--- index.js
+++ index.js
@@ -40,39 +40,10 @@ const init = async () => {
   });
 
   app.get("/video/:filename", (req, res) => {
-    // Ensure there is a range given for the video
-    const range = req.headers.range;
-    if (!range) {
-      res.status(400).send("Requires Range header");
-    }
-
-    // get video stats (about 61MB)
-    const videoPath = `./public/uploads/${req.params.filename}`;
-    const videoSize = statSync(videoPath).size;
-
-    // Parse Range
-    // Example: "bytes=32324-"
-    const CHUNK_SIZE = 10 ** 6; // 1MB
-    const start = Number(range.replace(/\D/g, ""));
-    const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
-
-    // Create headers
-    const contentLength = end - start + 1;
-    const headers = {
-      "Content-Range": `bytes ${start}-${end}/${videoSize}`,
-      "Accept-Ranges": "bytes",
-      "Content-Length": contentLength,
-      "Content-Type": "video/webm",
-    };
-
-    // HTTP Status 206 for Partial Content
-    res.writeHead(206, headers);
-
-    // create video read stream for this particular chunk
-    const videoStream = createReadStream(videoPath, { start, end });
-
-    // Stream the video chunk to the client
-    videoStream.pipe(res);
+    const { filename } = req.params
+    const videoFile = fs.readFileSync(`./public/uploads/${filename}`)
+    var encoded = new Buffer(videoFile).toString('base64')
+    res.json({ video: encoded })
   });
 
   app.get("/api/getEntries", async (req, res) => {
